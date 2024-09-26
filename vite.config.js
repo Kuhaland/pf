476de1d93b284c95ebca1813be8ccf396c0404ad
leehx78 <leehx78@smartscore.kr>
2024-09-26 15:34:33 +0900

    fixed

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/pf/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/pf/',
 });
