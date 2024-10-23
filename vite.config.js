7c76f26e656e59a3c481f3e1aca47f783fad3ba6
leehx78 <leehx78@smartscore.kr>
2024-10-23 12:21:47 +0900

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,10 +8,13 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   },
-  base: import.meta.env.MODE === 'production' ? '/pf/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/pf/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
   }
 });
