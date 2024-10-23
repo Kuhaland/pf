e1f402323065d9b9be03a85194ea9efb675034b4
leehx78 <leehx78@smartscore.kr>
2024-10-23 11:57:00 +0900

    fixed

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   },
-  base: '/pf/',
+  base: '/kuhaland.github.io/pf/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
