125bb386cddb156c6c3db6a78af9a54da9d4129a
leehx78 <leehx78@smartscore.kr>
2024-10-30 15:09:55 +0900

    fixed

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      1111
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
+    <a href="https://vuejs.org/" target="_blank">test
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
